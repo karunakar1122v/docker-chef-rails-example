371627b86313a03209a82abeef321608cbc26884
Austen Ito <austen.ito@gmail.com>
2014-07-28 08:39:23 -0400

    Moved config files into default.
    * Use relative paths in the data-volume recipe

--- cookbooks/example-cookbook/recipes/data-volume.rb
+++ cookbooks/example-cookbook/recipes/data-volume.rb
@@ -5,9 +5,33 @@
 
 include_recipe 'docker'
 
+cookbook_file 'Dockerfile' do
+  path '/tmp/Dockerfile'
+  source 'data-volume/Dockerfile'
+end
+
+directory('/tmp/rails-example') { action :create }
+
+cookbook_file 'rails-example/run.sh' do
+  path '/tmp/rails-example/run.sh'
+  source 'rails-example/run.sh'
+end
+
+directory('/tmp/nginx') { action :create }
+
+cookbook_file 'nginx/run.sh' do
+  path '/tmp/nginx/run.sh'
+  source 'nginx/run.sh'
+end
+
+cookbook_file 'nginx/nginx.conf' do
+  path '/tmp/nginx/nginx.conf'
+  source 'nginx/nginx.conf'
+end
+
 docker_image 'ubuntu' do
   tag 'data-volume'
-  source 'https://raw.githubusercontent.com/austenito/docker-chef-rails-example/master/cookbooks/example-cookbook/data-volume/Dockerfile'
+  source '/tmp'
   action :build_if_missing
 end
 
