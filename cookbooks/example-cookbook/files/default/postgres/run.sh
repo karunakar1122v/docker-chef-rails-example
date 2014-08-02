2f69b7cb4587e8c49269d9569af89583c5784399
Austen Ito <austen.ito@gmail.com>
2014-08-02 18:08:38 -0400

    Update postgres to point to the hba_conf file

--- cookbooks/example-cookbook/files/default/postgres/run.sh
+++ cookbooks/example-cookbook/files/default/postgres/run.sh
@@ -3,6 +3,7 @@ set -e
 
 POSTGRES_BIN_PATH=/usr/lib/postgresql/9.3/bin
 POSTGRES_CONFIG_FILE=/config/postgres/postgresql.conf
+POSTGRES_HBA_FILE=/config/postgres/pg_hba.conf
 POSTGRES_DATA=/pgdata
 
 
@@ -15,4 +16,4 @@ if find $POSTGRES_DATA -maxdepth 0 -empty | read v; then
     sudo -u postgres $POSTGRES_BIN_PATH/postgres --single --config-file=$POSTGRES_CONFIG_FILE <<< "CREATE DATABASE $POSTGRES_USER OWNER $POSTGRES_USER;"
 fi
 
-exec sudo -u postgres $POSTGRES_BIN_PATH/postgres --config-file=$POSTGRES_CONFIG_FILE
+exec sudo -u postgres $POSTGRES_BIN_PATH/postgres --config-file=$POSTGRES_CONFIG_FILE --hba_file=$POSTGRES_HBA_FILE
