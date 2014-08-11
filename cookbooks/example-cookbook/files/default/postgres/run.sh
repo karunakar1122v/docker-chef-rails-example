8d9c588bd85f6bc0fa7740ac822de8d7b536c621
Austen Ito <austen.ito@gmail.com>
2014-08-10 22:17:48 -0400

    Add configuration files for postgres image

--- cookbooks/example-cookbook/files/default/postgres/run.sh
+++ cookbooks/example-cookbook/files/default/postgres/run.sh
@@ -2,8 +2,6 @@
 set -e
 
 POSTGRES_BIN_PATH=/usr/lib/postgresql/9.3/bin
-POSTGRES_CONFIG_FILE=/config/postgres/postgresql.conf
-POSTGRES_HBA_FILE=/config/postgres/pg_hba.conf
 POSTGRES_DATA=/pgdata
 
 
@@ -11,9 +9,9 @@ if find $POSTGRES_DATA -maxdepth 0 -empty | read v; then
     mkdir -p $POSTGRES_DATA
     chown -R postgres:postgres $POSTGRES_DATA
     sudo -u postgres $POSTGRES_BIN_PATH/initdb -D $POSTGRES_DATA -E UTF8
-    sudo -u postgres $POSTGRES_BIN_PATH/postgres --single --config-file=$POSTGRES_CONFIG_FILE <<< "CREATE USER $POSTGRES_USER WITH SUPERUSER;"
-    sudo -u postgres $POSTGRES_BIN_PATH/postgres --single --config-file=$POSTGRES_CONFIG_FILE <<< "ALTER USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
-    sudo -u postgres $POSTGRES_BIN_PATH/postgres --single --config-file=$POSTGRES_CONFIG_FILE <<< "CREATE DATABASE $POSTGRES_USER OWNER $POSTGRES_USER;"
+    sudo -u postgres $POSTGRES_BIN_PATH/postgres --single <<< "CREATE USER $POSTGRES_USER WITH SUPERUSER;"
+    sudo -u postgres $POSTGRES_BIN_PATH/postgres --single <<< "ALTER USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+    sudo -u postgres $POSTGRES_BIN_PATH/postgres --single  <<< "CREATE DATABASE $POSTGRES_USER OWNER $POSTGRES_USER;"
 fi
 
-exec sudo -u postgres $POSTGRES_BIN_PATH/postgres --config-file=$POSTGRES_CONFIG_FILE --hba_file=$POSTGRES_HBA_FILE
+exec sudo -u postgres $POSTGRES_BIN_PATH/postgres
