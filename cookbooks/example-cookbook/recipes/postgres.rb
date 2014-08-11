32b413b327ef364d6be22e4c0abe0811ddc9f876
Austen Ito <austen.ito@gmail.com>
2014-08-11 18:53:22 -0400

    Update postgres cookbook

--- cookbooks/example-cookbook/recipes/postgres.rb
+++ cookbooks/example-cookbook/recipes/postgres.rb
@@ -6,20 +6,29 @@
 
 include_recipe 'docker'
 
+remote_directory '/tmp/postgres' do
+  source 'postgres'
+end
+
+docker_image 'austenito/ruby' do
+  source '/tmp/postgres'
+  action :build_if_missing
+end
+
 if `sudo docker ps -a | grep postgres`.size > 0
   execute('stop container') { command "docker stop -t 60 postgres" }
   execute('remove container') { command "docker rm -f postgres" }
 end
 
 docker_container 'postgres' do
-  image 'ubuntu:postgres'
+  image 'austenito/ruby'
   container_name 'postgres'
   port "5432:5432"
   detach true
-  env ["POSTGRES_USER=#{node['postgres']['user']}",
-       "POSTGRES_PASSWORD=#{node['postgres']['password']}"
+  env ["POSTGRES_USER=#{node['postgresql']['user']}",
+       "POSTGRES_PASSWORD=#{node['postgresql']['password']}"
       ]
   volumes_from 'data-volume'
   action :run
-  command '/config/postgres/run.sh'
+  command '/tmp/postgres/run.sh'
 end
