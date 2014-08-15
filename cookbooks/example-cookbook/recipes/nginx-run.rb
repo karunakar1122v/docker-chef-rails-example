b51fb264b9351a5714eba7d6b51bcdd65da02b06
Austen Ito <austen.ito@gmail.com>
2014-08-15 14:51:04 -0400

    Postgres is built using chef when creating a docker image

--- cookbooks/example-cookbook/recipes/nginx-run.rb
+++ cookbooks/example-cookbook/recipes/nginx-run.rb
@@ -5,6 +5,21 @@
 
 include_recipe 'docker'
 
+# how to change your configuration without rebuilding the image?
+#
+# the configuration needs to live someplace else
+#
+# where should this file live? in the nginx image? no. It should be in the copy the template to the 
+# if i want to change config
+#   * shutdown the containers
+#   * rebuild the image with the new config
+#   *
+#
+# Copy docker file and nginx config template to tmp
+# Build nginx image here. build if missing.
+#
+# you can fiddle with the config in the data-volume. Do I want to have a nginx volume?
+
 if `sudo docker ps -a | grep nginx`.size > 0
   execute('stop container') { command "docker stop -t 60 nginx" }
   execute('remove container') { command "docker rm -f nginx" }
--- cookbooks/example-cookbook/recipes/postgres.rb
+++ cookbooks/example-cookbook/recipes/postgres.rb
@@ -10,18 +10,19 @@ remote_directory '/tmp/postgres' do
   source 'postgres'
 end
 
-docker_image 'austenito/ruby' do
+docker_image 'austenito/postgres' do
   source '/tmp/postgres'
+  tag '9.3'
   action :build_if_missing
+  cmd_timeout 900
 end
 
 if `sudo docker ps -a | grep postgres`.size > 0
   execute('stop container') { command "docker stop -t 60 postgres" }
-  execute('remove container') { command "docker rm -f postgres" }
 end
 
 docker_container 'postgres' do
-  image 'austenito/ruby'
+  image 'austenito/postgres:9.3'
   container_name 'postgres'
   port "5432:5432"
   detach true
@@ -30,5 +31,4 @@ docker_container 'postgres' do
       ]
   volumes_from 'data-volume'
   action :run
-  command '/tmp/postgres/run.sh'
 end
